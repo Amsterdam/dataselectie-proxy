eaabb06a6b1f5fa188e38bdb2b1bd06faad5c987
Maguell Sandifort <maguell.sandifort@amsterdam.nl>
2025-06-19 14:18:15 +0200

    Remove v1 prefix from path to prevent clash with dso ingress

--- src/dataselectie_proxy/search/urls.py
+++ src/dataselectie_proxy/search/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path(
-        "v1/dataselectie/<str:dataset_name>/search",
+        "dataselectie/<str:dataset_name>/search",
         views.ProxySearchView.as_view(),
         name="dataselectie-index",
     )
